b0fcb7470b32d4516971714b247d5dd23f1c196c
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2022-10-22 14:40:58 -0300

    requisitos até o 6 ok

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
+
+class SolarSystem extends Component {
+  render() {
+    const planetsList = planets.map((planet, index) => (
+      <li key={ index }>
+        <PlanetCard
+          planetName={ planet.name }
+          planetImage={ planet.image }
+        />
+      </li>));
+    return (
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+        <ul>
+          { planetsList }
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
