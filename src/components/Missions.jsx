516b206bf8df6b1ebd768551ff34ef3c613b16b6
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2022-10-22 15:14:15 -0300

    requisitos completos faltando o css

---
+++ src/components/Missions.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
+
+class Missions extends Component {
+  render() {
+    const missionsList = missions.map((mission, index) => (
+      <li key={ index }>
+        <MissionCard
+          name={ mission.name }
+          year={ mission.year }
+          country={ mission.country }
+          destination={ mission.destination }
+        />
+      </li>));
+    return (
+      <div data-testid="missions">
+        <Title headline="Missões" />
+        { missionsList }
+      </div>
+    );
+  }
+}
+
+export default Missions;
