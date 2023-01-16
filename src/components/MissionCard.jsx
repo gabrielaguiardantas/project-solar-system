d8442e52c6a034fd2008ba1b075d2114e0838d32
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2023-01-16 11:27:44 -0300

    subindo projeto 8

--- src/components/MissionCard.jsx
+++ src/components/MissionCard.jsx
@@ -6,10 +6,27 @@ class MissionCard extends Component {
     const { name, year, country, destination } = this.props;
     return (
       <div data-testid="mission-card">
-        <p data-testid="mission-name">{ name }</p>
-        <p data-testid="mission-year">{ year }</p>
-        <p data-testid="mission-country">{ country }</p>
-        <p data-testid="mission-destination">{ destination }</p>
+        <p data-testid="mission-name">
+          Nome:
+          {' '}
+          { name }
+        </p>
+        <p data-testid="mission-year">
+          Ano:
+          {' '}
+          { year }
+        </p>
+        <p data-testid="mission-country">
+          País:
+          {' '}
+          { country }
+        </p>
+        <p data-testid="mission-destination">
+          Destino:
+          {' '}
+          { destination }
+        </p>
+        <br />
       </div>
     );
   }
